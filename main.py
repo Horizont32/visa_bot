9fcc58ca0c3f64f03e53e01a14bde11cb310f570
Artem Golovachev <artbryansk94@gmail.com>
2023-06-26 00:41:15 +0300

    Appointment details progress

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 import time
 from pypasser import reCaptchaV2, reCaptchaV3
 import configparser
-
+import undetected_chromedriver as uc
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 # Importing config
@@ -17,6 +18,8 @@ URL = config.get('LoginData', 'url')
 EMAIL = config.get('LoginData', 'email')
 PASSWORD = config.get('LoginData', 'pwd')
 driver = webdriver.Chrome()
+# driver = uc.Chrome(driver_executable_path='/Users/artemgolovachev/bin/chromedriver2')
+
 
 driver.get(URL)
 el = WebDriverWait(driver, 100).until(lambda p: p.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]'))
@@ -29,19 +32,33 @@ pass_form = driver.find_element(By.ID, 'mat-input-1').send_keys(PASSWORD)
 # is_checked = reCaptchaV2(driver) # it returns bool
 is_checked = reCaptchaV3(el.get_attribute('src'))
 query = f"___grecaptcha_cfg.clients['0'].O.O.callback('{is_checked}');"
-print(query)
+# print(query)
 driver.execute_script(query)
 # is_checked = reCaptchaV3('https://www.recaptcha.net/recaptcha/api2/anchor?ar=1&k=6LfDUY8bAAAAAPU5MWGT_w0x5M-8RdzC29SClOfI&co=aHR0cHM6Ly92aXNhLnZmc2dsb2JhbC5jb206NDQz&hl=en&v=SglpK98hSCn2CroR0bKRSJl5&size=normal&cb=v3n9p9ydwh61')
-print(is_checked)
+# print(is_checked)
 driver.execute_script(f'document.getElementById("g-recaptcha-response").innerHTML="{is_checked}";')
 driver.execute_script(f'document.getElementById("g-recaptcha-response-100000").innerHTML="{is_checked}";')
 time.sleep(2)
 driver.find_element(By.XPATH, '//form/button').click()
-time.sleep(6)
-start_app_btn = driver.find_element(By.XPATH, '//app-dashboard/section[1]/div/div[1]/div[@class="position-relative"]/button')
+
+#Dashboard page
+time.sleep(4)
+start_app_btn = driver.find_element(By.XPATH, '//app-dashboard/section[1]/div/div[@class="position-relative"]/button')
 print(start_app_btn.text, '------')
 if start_app_btn:
     start_app_btn.click()
+
+#Appointment details page
+time.sleep(4)
+
+location_selector = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="centerCode"]')
+location_selector.click()
+time.sleep(3)
+location_selector_reg = driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-6"]').click()
+time.sleep(3)
+visaCat = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="selectedSubvisaCategory"]')
+visaCatCode = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="visaCategoryCode"]')
+
 print(dir(el))
 print(el.get_attribute('src'))
 captcha_key = el.get_attribute('src').split('&')
