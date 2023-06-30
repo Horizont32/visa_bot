3139a080c5be739ca6140bc1c1e22560ab9e9b8c
Golovachev Artem <42933596+Horizont32@users.noreply.github.com>
2023-06-30 18:47:56 +0300

    Switch to a text instead of mat-optiond ids

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ driver = uc.Chrome()
 
 driver.get(URL)
 el = WebDriverWait(driver, 100).until(lambda p: p.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]'))
-# Create an instance of webdriver and open the page has recaptcha v2
-# ...
+
 time.sleep(8)
 
 
@@ -36,11 +35,11 @@ if cookie_acceptor:
 
 email_form = driver.find_element(By.ID, 'mat-input-0').send_keys(EMAIL)
 pass_form = driver.find_element(By.ID, 'mat-input-1').send_keys(PASSWORD)
-# pass the driver to reCaptchaV2
+
 # is_checked = reCaptchaV2(driver) # it returns bool
 is_checked = reCaptchaV3(el.get_attribute('src'))
 query = f"___grecaptcha_cfg.clients['0'].O.O.callback('{is_checked}');"
-# print(query)
+
 driver.execute_script(query)
 # is_checked = reCaptchaV3('https://www.recaptcha.net/recaptcha/api2/anchor?ar=1&k=6LfDUY8bAAAAAPU5MWGT_w0x5M-8RdzC29SClOfI&co=aHR0cHM6Ly92aXNhLnZmc2dsb2JhbC5jb206NDQz&hl=en&v=SglpK98hSCn2CroR0bKRSJl5&size=normal&cb=v3n9p9ydwh61')
 # print(is_checked)
@@ -55,8 +54,6 @@ time.sleep(4)
 #Unix-based driver implementation
 start_app_btn = driver.find_element(By.XPATH, '//app-dashboard/section[1]/div/div[@class="position-relative"]/button')
 
-#Windows driver implementation
-# start_app_btn = driver.find_element(By.XPATH, '//div[@class="col-12 col-sm-auto d-lg-none"]/button')
 print(start_app_btn.tag_name, '------')
 if start_app_btn:
     start_app_btn.click()
@@ -66,13 +63,12 @@ time.sleep(4)
 
 location_selector = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="centerCode"]')
 location_selector.click()
-time.sleep(1)
+# time.sleep(1)
 # Unix-based driver implementation
-# driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-6"]').click()
-WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-6"]')).click()
 
-#Windows driver implementation
-# driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-option-6"]/span'))))
+# WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-6"]')).click()
+WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//*[contains(text(), "Moscow")]')).click()
+
 
 time.sleep(3)
 
@@ -80,31 +76,30 @@ time.sleep(3)
 # visaCat = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="selectedSubvisaCategory"]')
 visaCat = WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//mat-select[@formcontrolname="selectedSubvisaCategory"]'))
 visaCat.click()
-time.sleep(1)
+# time.sleep(1)
 # Unix-based driver implementation
-WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-20"]')).click()
-# driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-20"]').click()
+# WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-20"]')).click()
+WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//*[contains(text(), "Short")]')).click()
 
-#Windows driver implementation
-# driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-option-20"]/span'))))
 
 time.sleep(3)
 
 visaCatCode = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="visaCategoryCode"]')
 visaCatCode.click()
 # driver.execute_script("arguments[0].click();", visaCatCode)
-time.sleep(1)
+# time.sleep(1)
 print('before executing last checkbox script')
 # Unix-based driver implementation
-driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-23"]').click()
+# driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-23"]').click()  @ SHort stay all other
+# driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-31"]').click()  # French parent
+# WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//*[contains(text(), "All kind")]')).click()
+WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//*[contains(text(), "French child")]')).click()
 
-#Windows driver implementation
-# driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-option-23"]/span'))))
 
 time.sleep(3)
 
 continue_btn = driver.find_element(By.XPATH, '//app-eligibility-criteria/section/form/mat-card[2]/button')
-print(continue_btn.tag_name, '------')
+
 if continue_btn:
     continue_btn.click()
 
@@ -131,7 +126,5 @@ driver.find_element(By.XPATH, '//input[@type="email"]').send_keys(config.get('Lo
 
 save_btn = driver.find_element(By.XPATH, '//app-dynamic-control/div/div/div[2]/button').click()
 time.sleep(10)
-# print(dir(el))
-# print(el.get_attribute('src'))
-# captcha_key = el.get_attribute('src').split('&')
+
 driver.quit()
