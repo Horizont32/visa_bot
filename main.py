caf136259a082606b80dc685bf819ba1b51e7807
Artem Golovachev <artbryansk94@gmail.com>
2023-07-02 23:15:50 +0300

    Bot can get through all the process until payment data is needed

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import time
 from pypasser import reCaptchaV2, reCaptchaV3
 import configparser
 import undetected_chromedriver as uc
+from utils import callback_execute
+
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,13 +21,18 @@ config.read('cfg.ini')
 URL = config.get('LoginData', 'url')
 EMAIL = config.get('LoginData', 'email')
 PASSWORD = config.get('LoginData', 'pwd')
-# driver = webdriver.Chrome()
-driver = uc.Chrome()
 
+# chrome_options = Options()
+# chrome_options.add_argument('--headless=new')
 
+driver = webdriver.Chrome()
+# driver = webdriver.Chrome(options=chrome_options)
+# driver = uc.Chrome()
+
+driver.maximize_window()
 driver.get(URL)
-el = WebDriverWait(driver, 100).until(lambda p: p.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]'))
-
+el = WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]'))
+print('wwqew')
 time.sleep(8)
 
 
@@ -38,13 +46,14 @@ pass_form = driver.find_element(By.ID, 'mat-input-1').send_keys(PASSWORD)
 
 # is_checked = reCaptchaV2(driver) # it returns bool
 is_checked = reCaptchaV3(el.get_attribute('src'))
-query = f"___grecaptcha_cfg.clients['0'].O.O.callback('{is_checked}');"
+# query = f"___grecaptcha_cfg.clients['0'].O.O.callback('{is_checked}');"
+# query = f"___grecaptcha_cfg.clients['0'].Z.Z.callback('{is_checked}');"
+callback_execute(driver, is_checked)
 
-driver.execute_script(query)
 # is_checked = reCaptchaV3('https://www.recaptcha.net/recaptcha/api2/anchor?ar=1&k=6LfDUY8bAAAAAPU5MWGT_w0x5M-8RdzC29SClOfI&co=aHR0cHM6Ly92aXNhLnZmc2dsb2JhbC5jb206NDQz&hl=en&v=SglpK98hSCn2CroR0bKRSJl5&size=normal&cb=v3n9p9ydwh61')
 # print(is_checked)
-driver.execute_script(f'document.getElementById("g-recaptcha-response").innerHTML="{is_checked}";')
-driver.execute_script(f'document.getElementById("g-recaptcha-response-100000").innerHTML="{is_checked}";')
+driver.execute_script(f'document.getElementById("g-recaptcha-response").value="{is_checked}";')
+driver.execute_script(f'document.getElementById("g-recaptcha-response-100000").value="{is_checked}";')
 time.sleep(2)
 driver.find_element(By.XPATH, '//form/button').click()
 
@@ -63,7 +72,7 @@ time.sleep(4)
 
 location_selector = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="centerCode"]')
 location_selector.click()
-# time.sleep(1)
+time.sleep(2)
 # Unix-based driver implementation
 
 # WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-6"]')).click()
@@ -87,8 +96,7 @@ time.sleep(3)
 visaCatCode = driver.find_element(By.XPATH, '//mat-select[@formcontrolname="visaCategoryCode"]')
 visaCatCode.click()
 # driver.execute_script("arguments[0].click();", visaCatCode)
-# time.sleep(1)
-print('before executing last checkbox script')
+time.sleep(1)
 # Unix-based driver implementation
 # driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-23"]').click()  @ SHort stay all other
 # driver.find_element(By.XPATH, '//div[@role="listbox"]/mat-option[@id="mat-option-31"]').click()  # French parent
@@ -116,15 +124,76 @@ driver.find_element(By.ID, 'dateOfBirth').send_keys(config.get('AppointmentData'
 driver.find_element(By.XPATH, '//app-dynamic-form/div/div/app-dynamic-control[5]/div/div/div/app-dropdown/div/mat-form-field/div/div[1]/div[3]/mat-select').click()
 time.sleep(3)
 
-driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/div/div/mat-option[172]').click()
+# driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/div/div/mat-option[172]').click()
+driver.find_element(By.XPATH, '//*[contains(text(), "RUSSIAN FEDERATION")]').click()
 driver.find_element(By.XPATH, '//input[@data-placeholder="Enter passport number"]').send_keys(config.get('AppointmentData', 'pnum'))
 driver.find_element(By.ID, 'passportExpirtyDate').send_keys(config.get('AppointmentData', 'pex'))
 driver.find_element(By.XPATH, '//input[@data-placeholder="44"]').send_keys(config.get('AppointmentData', 'phonecode'))
 driver.find_element(By.XPATH, '//input[@data-placeholder="012345648382"]').send_keys(config.get('AppointmentData', 'phonenum'))
 driver.find_element(By.XPATH, '//input[@type="email"]').send_keys(config.get('LoginData', 'email'))
 
+time.sleep(19)
 
 save_btn = driver.find_element(By.XPATH, '//app-dynamic-control/div/div/div[2]/button').click()
-time.sleep(10)
+time.sleep(4)
+
+# Page with applicants before the slot booking
+
+cont_btn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//mat-card[2]/div/div[2]/button')))
+driver.execute_script("arguments[0].click();", cont_btn)
+time.sleep(4)
+
+# Page with calendar and dates availiable current month
+
+dates = WebDriverWait(driver, 5).until(lambda p: p.find_elements(By.XPATH, '//td[contains(@class, "date-availiable")]'))
+for date in dates:
+    print(date.get_attribute('data-date'))
+
+dates[-1].click()
+
+times = WebDriverWait(driver, 5).until(lambda p: p.find_elements(By.XPATH, '//input[@name="SlotRadio"]'))
+print([time_order.value for time_order in times])
+times[-1].click()
+
+
+# Entering the fees state
+
+cont_btn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//mat-card[2]/div/div[2]/button')))
+driver.execute_script("arguments[0].click();", cont_btn)
+time.sleep(3)
+
+# Go for an insurance page and skip
+cont_btn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//mat-card[2]/div/div[2]/button')))
+driver.execute_script("arguments[0].click();", cont_btn)
+time.sleep(3)
+
+cont_btn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//mat-card[2]/div/div[2]/button')))
+driver.execute_script("arguments[0].click();", cont_btn)
+time.sleep(1)
+
+# Skip warning
+skip_btn = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@role="dialog"]/div/div[2]/div[2]/button')))
+# driver.execute_script("arguments[0].click();", cont_btn)
+skip_btn.click()
+time.sleep(5)
+
+# Review page
+accept_checkbox = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="pl-35"]/mat-checkbox')))
+accept_checkbox.click()
+time.sleep(5)
+
+# Pay btn
+pay_online = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//mat-card[2]/div/div[2]/button')))
+driver.execute_script("arguments[0].click();", pay_online)
+time.sleep(3)
+
+# Disclaimer
+disclaimer = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//mat-card/div[2]/div[2]/button')))
+driver.execute_script("arguments[0].click();", disclaimer)
+time.sleep(8)
+
+
+
+time.sleep(40)
 
 driver.quit()
---
+++ utils.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+import configparser, time
+
+def callback_execute(driver: WebDriver, captcha_token: str):
+    recaptcha_query = """
+    function findRecaptchaClients() {
+  // eslint-disable-next-line camelcase
+  if (typeof (___grecaptcha_cfg) !== 'undefined') {
+    // eslint-disable-next-line camelcase, no-undef
+    return Object.entries(___grecaptcha_cfg.clients).map(([cid, client]) => {
+      const data = { id: cid, version: cid >= 10000 ? 'V3' : 'V2' };
+      const objects = Object.entries(client).filter(([_, value]) => value && typeof value === 'object');
+
+      objects.forEach(([toplevelKey, toplevel]) => {
+        const found = Object.entries(toplevel).find(([_, value]) => (
+          value && typeof value === 'object' && 'sitekey' in value && 'size' in value
+        ));
+     
+        if (typeof toplevel === 'object' && toplevel instanceof HTMLElement && toplevel['tagName'] === 'DIV'){
+            data.pageurl = toplevel.baseURI;
+        }
+        
+        if (found) {
+          const [sublevelKey, sublevel] = found;
+
+          data.sitekey = sublevel.sitekey;
+          const callbackKey = data.version === 'V2' ? 'callback' : 'promise-callback';
+          const callback = sublevel[callbackKey];
+          if (!callback) {
+            data.callback = null;
+            data.function = null;
+          } else {
+            data.function = callback;
+            const keys = [cid, toplevelKey, sublevelKey, callbackKey].map((key) => `['${key}']`).join('');
+            console.log(keys)
+            data.callback = `___grecaptcha_cfg.clients${keys}` + "('tkn')";
+          }
+        }
+      });
+      return data;
+    });
+  }
+  return [];
+}
+
+let res = findRecaptchaClients()[0].callback;
+return res
+
+    """
+    query = driver.execute_script(recaptcha_query)
+    if not query:
+        return False
+    query = query.replace('tkn', captcha_token)
+    driver.execute_script(query)
+
+    return True
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('cfg.ini')
+    URL = config.get('LoginData', 'url')
+    EMAIL = config.get('LoginData', 'email')
+    PASSWORD = config.get('LoginData', 'pwd')
+
+    # chrome_options = Options()
+    # chrome_options.add_argument('--headless=new')
+
+    driver = webdriver.Chrome()
+    # driver = webdriver.Chrome(options=chrome_options)
+    # driver = uc.Chrome()
+
+
+    driver.get(URL)
+    el = WebDriverWait(driver, 10).until(lambda p: p.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]'))
+    print('wwqew')
+    time.sleep(8)
+    print(callback_execute(driver))
